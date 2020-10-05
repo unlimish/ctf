da5b4a675801308c37ba1bb4931015360c6eb946
unlimish <mail@unlimish.xyz>
2020-10-05 16:52:03 +0900

    Add the_number

---
+++ picoCTF/the_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	const char	str = '@';
+	printf("%c", str + 20);
+	printf("%c", str + 8);
+	printf("%c", str + 5);
+	printf("%c", str + 14);
+	printf("%c", str + 21);
+	printf("%c", str + 13);
+	printf("%c", str + 2);
+	printf("%c", str + 5);
+	printf("%c", str + 18);
+	printf("%c", str + 19);
+	printf("%c", str + 13);
+	printf("%c", str + 1);
+	printf("%c", str + 19);
+	printf("%c", str + 15);
+	printf("%c", str + 14);
+}
